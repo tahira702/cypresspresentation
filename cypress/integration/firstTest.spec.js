ce2c0a1a17a37a66b292f5c037d85b0fcd7a4c47
tahira702 <65274431+tahira702@users.noreply.github.com>
2021-07-08 12:53:56 +0500

    visit not working

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,19 @@
+
+/// <reference types="cypress"/>
+
+    describe('Demo Test inside', ()=> {
+        it('my first Test', ()=>{
+            cy.visit('/pages')
+            cy.contains('Forms').click()
+            cy.contains('Form Layouts').click()
+            cy.get('input') //Tag name
+            cy.get('#inputEmail1').type('taka@ciklum.com') // by Id
+            cy.get('.input-full-width') // by Class name
+            cy.get('[placeholder]') //by attribute name
+            cy.get('[placeholder="Email"]') //by attribute name and value
+            cy.get('[class="input-full-width size-medium shape-rectangle"]') // by Class value
+            cy.get('[data-cy="imputEmail1"]')// custom attribute
+        })
+
+    })
+
