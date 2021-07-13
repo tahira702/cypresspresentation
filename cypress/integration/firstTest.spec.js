e67015aaef339e2f58abd382c3e8872825714520
tahira702 <65274431+tahira702@users.noreply.github.com>
2021-07-13 16:46:45 +0500

    submit form

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,6 +1,8 @@
 
 /// <reference types="cypress"/>
 
+
+
     describe('Demo Test inside', ()=> {
         it('my first Test', ()=>{
             cy.visit('/')
@@ -8,12 +10,17 @@
             cy.contains('Form Layouts').click()
             cy.wait(5000)
             cy.get('input') //Tag name
-            cy.get('#inputEmail1').type('taka@ciklum.com') // by Id
+            cy.get('#inputEmail1') // by Id
             cy.get('.input-full-width') // by Class name
             cy.get('[placeholder]') //by attribute name
             cy.get('[placeholder="Email"]') //by attribute name and value
             cy.get('[class="input-full-width size-medium shape-rectangle"]') // by Class value
             cy.get('[data-cy="imputEmail1"]').type('taka@ciklum.com')// custom attribute
+            cy.get('[data-cy=signInButton]')
+            cy.get('#inputPassword2').type('1234')
+            cy.get('nb-radio')
+            cy.get('#inputEmail1').parents('form').find('button').click({ force: true })
+            
             
             
         })
